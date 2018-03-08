ca6ba7de35e984b90bbb94799880ec9593f2c8ec
bennettmajer <35977336+bennettmajer@users.noreply.github.com>
2018-03-07 21:44:38 -0600

    final

--- DormGUI.java
+++ DormGUI.java
@@ -1,10 +1,17 @@
-
+/**
+ * (This Interface defines an implementation where an object will have the ability to
+ * have it's location and appearance updated, do something when it is clicked, revert to 
+ * doing nothing when the mouse is released, and tells when the mouse is over it.)
+ * <p>
+ * Classes implementing: (LoadButton.java, ClearButton.java, CreateFurnitureButton.java
+ * SaveButton.java, Button.java, Furniture.java)
+ * <p>
+ * Bugs: (no known bugs)
+ * @author (Bennett Majerowski and John Gibson)
+ */
 public interface DormGUI {
-
 	public void update();
 	public void mouseDown(Furniture[] furniture);
 	public void mouseUp();
 	public boolean isMouseOver();
-	
-	
 }
