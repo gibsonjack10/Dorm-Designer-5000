7d302508804a7bf4e3535a2fde850d1a638530ad
bennettmajer <35977336+bennettmajer@users.noreply.github.com>
2018-03-08 07:50:49 -0600

    new create furniture button

--- CreateFurnitureButton.java
+++ CreateFurnitureButton.java
@@ -1,35 +1,45 @@
 /**
- * (This class creates a button to clear all furniture from the room.)
- *
- * <p>Bugs: (no known bugs)
- *
+ * (This class creates a button to make beds.)
+ * <p>
+ * Subclass of: (Button.java)
+ * <p>
+ * Bugs: (no known bugs)
  * @author (Bennett Majerowski and John Gibson)
  */
-public class ClearButton extends Button implements DormGUI {
+public class CreateFurnitureButton extends Button implements DormGUI {
+	private PApplet processing;
 	private float[] position;
+	private String type;
 	
 	/**
-	 * Constructor that creates the Clear Button
+	 * Constructor that creates the bed button
 	 * @param x is the horizontal location of the button
 	 * @param y is the vertical location of the button
 	 * @param processing uses information from the jar file
 	 */
-	public ClearButton(float x, float y, PApplet processing) {
+	public CreateFurnitureButton(String type, float x, float y, PApplet processing) {
 		super(x,y,processing);
 		this.processing = processing;
 		position = new float[2];
 		position[0] = x;
 		position[1] = y;
-		label = setLabel("Clear Room");
+		this.type = type;
+		label = setLabel("Create " + type);
 	}
 	
 	/**
-	 * (mouseDown deletes all references in the furniture array)
+	 * (mouseDown identifies the type "bed" if the mouse is clicked while above the 
+	 * Create sofa button.)
 	 */
 	@Override
 	public void mouseDown(Furniture[] furniture) {
 		for (int i=0; i<furniture.length; i++) {
-			furniture[i] = null;
+			if (furniture[i] == null) {
+				Furniture furn = new Furniture(type, processing);
+				furniture[i] = furn;
+				break;
+			}
 		}
 	}
 }
+
