0ce96ff8ba95289f77670b00bfea5f92af69e857
bennettmajer <35977336+bennettmajer@users.noreply.github.com>
2018-03-07 21:45:26 -0600

    final

--- LoadButton.java
+++ LoadButton.java
@@ -1,53 +1,58 @@
 import java.io.File;
 import java.util.Scanner;
-//import java.io.FileNotFoundException;
+import java.io.FileNotFoundException;
 
-public class LoadButton implements DormGUI {
-	private static final int WIDTH = 96;
-	private static final int HEIGHT = 32;
-
+/**
+ * This class Loads a previously saved arrangement of furniture into the
+ * dorm room.  It checks for any errors in what type of furniture is put
+ * in, that the correct file is loaded and that the style is correct.
+ * <p>
+ * Subclass of: (Button.java)
+ * <p>
+ * @author John Gibson and Bennett Majerowski
+ *
+ */
+public class LoadButton extends Button implements DormGUI {
 	private PApplet processing;
 	private float[] position;
-	private String label;
 	private File f;
 	private File g;
-
+	
+/**
+ * Constructor that creates the Load Button
+ * @param x is the horizontal location of the button
+ * @param y is the vertical location of the button
+ * @param processing uses information from the jar file
+ */
 	public LoadButton(float x, float y, PApplet processing) {
+		super(x,y,processing);
 		this.processing = processing;
 		position = new float[2];
 		position[0] = x;
 		position[1] = y;
-		label = new String("Load Room");
+		label = setLabel("Load Room");
 	}
-
-
-
-
-
-
-	public void update() {
-		if (isMouseOver() == true) {
-			processing.fill(100);
-		} else {
-			processing.fill(200);
-		}
-		processing.rect(position[0] - WIDTH/2, position[1] + HEIGHT/2, position[0] + WIDTH/2, position[1] - HEIGHT/2);
-
-		processing.fill(0);
-		processing.text(label, position[0], position[1]);
-	}
-
-	public void mouseDown(Furniture furniture[]) /*throws FileNotFoundException*/ {
-		try {
+	
+/**
+ * this method is called when the mouse clicks on the load button and loads a
+ * previous state of the room that was saved
+ * @param furniture array used to put in the data about individual pieces of
+ * furniture
+ * @return returns the furniture arrays to the Furniture class so they can be
+ * constructed into pieces of furniture in our room
+ * @throws FileNotFoundException if the file isn't found and can't be loaded
+ */
+	@Override
+	public void mouseDown(Furniture[] furniture) {
+		try { // looks to make sure we are loading the correct file
 			if (isMouseOver()) {
 				f = new File("RoomData.ddd");
 				Scanner scan = new Scanner(f);
 				String finalLines = new String();
 				int i = 0;
 				int lineCounter = 0;
-				for (int j = 0; j < 6; j++) {
+				for (int j=0; j<6; j++)
 					furniture[j] = null;
-				}
 				while(scan.hasNextLine()) {
 					lineCounter++;
 					if (i > 5) {
@@ -59,59 +64,31 @@ public class LoadButton implements DormGUI {
 						finalLines = scan.nextLine();
 						lineCounter++;
 					}
-					try {
+					try { // checks for correct formatting then puts data into Furniture class
 						String trimLine = finalLines.trim();
-						String furnType = trimLine.split(":")[0];
-						g = new File("images/" + furnType.trim() + ".png");
-						if(!g.isFile()) {
-							System.out.println("WARNING: Could not find an image for a furniture object of type: " 
-									+ furnType.trim());
-						}
+						String furnType = trimLine.split(":")[0].trim();
+						g = new File("images/" + furnType + ".png");
 						String numbers = trimLine.split(":")[1];
 						String xPos = numbers.split(",")[0];
 						String yPos = numbers.split(",")[1];
 						String rotations = numbers.split(",")[2];
-						System.out.println(furnType + numbers);
-
-						furniture[i] = new Furniture(furnType.trim(), Float.parseFloat(xPos.trim()), Float.parseFloat(yPos.trim()),
+						if (!g.isFile()) { 
+							System.out.println("WARNING: Could not find an image for"
+									+ "a furniture object of type: " + furnType.trim());
+							continue;
+						}
+						furniture[i] = new Furniture(furnType.trim(), 
+								Float.parseFloat(xPos.trim()), Float.parseFloat(yPos.trim()), 
 								Integer.parseInt(rotations.trim()), processing);
 						i++;
 					} catch (Exception e) {
-						System.out.println("WARNING: Found incorrectly formatted line in file: " + lineCounter);
+						System.out.println("WARNING: Found incorrectly formatted line in file: "
+								+ lineCounter);
 					}
-					
-					
 				}
 				scan.close();
-
-			} else {
-				
 			}
 		} catch (Exception e) {
-			System.out.println("WARNING: Could not load room contents from file RoomData.ddd.");
 		}
-		
-
-	}
-
-	public boolean isMouseOver() {
-		if (processing.mouseX > position[0] - WIDTH/2 && 
-				processing.mouseX < position[0] + WIDTH/2 &&
-				processing.mouseY > position[1] - HEIGHT/2 &&
-				processing.mouseY < position[1] + HEIGHT/2) {
-			return true;
-		}
-		return false;	
-	}
-
-
-
-
-
-
-	
-	public void mouseUp() {
-		// TODO Auto-generated method stub
-		
 	}
 }
